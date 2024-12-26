8d2aa4e82117a336488a29ddc9e47fa236ec338f
Ehteshambutt <ehteshambutt58@gmail.com>
2024-12-26 12:17:28 +0500

    11 40

--- index.js
+++ index.js
@@ -89,15 +89,23 @@ app.use(function (req, res, next) {
 
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
 });
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Provide error details in development environment
+  const isDev = req.app.get("env") === "development";
+  res.locals.message = err.message;
+  res.locals.error = isDev ? err : {};
+
+  // Respond with the error view
+  res.status(err.status || 500);
+  res.render("error", { error: err });
+});
+
 
 app.get('/signup', function(req, res){
   res.render('signin');
--- routes/claimemail.js
+++ routes/claimemail.js
@@ -10,7 +10,36 @@ cloudinary.config({
   api_secret: 'FCqYSd-J1Kew_VgMCOBZSIcqnJY'
 
 });
-
+router.post(
+    "/",
+    (req, res, next) => {
+      const claimemail = new Claimemail({
+        _id: new mongoose.Types.ObjectId(), // Correct mongoose ObjectId usage
+        from: req.body.from,
+        mailgunResponseId: req.body.mailgunResponseId,
+        sentAt: req.body.sentAt,
+        subject: req.body.subject,
+        type: req.body.type,
+        to: req.body.to,
+        text: req.body.text,
+      });
+  
+      claimemail
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(200).json({
+            newClaimemail: result,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: err,
+          });
+        });
+    }
+  );
 router.get("/", (req, res, next) => {
     Claimemail.find()
     .then((result) => {
@@ -25,43 +54,7 @@ router.get("/", (req, res, next) => {
       });
     });
 });
-router.post("/",
-// checkAuth,
-(req, res, next) => {
-  const file=req.files.photo;
-  cloudinary.uploader.upload(file.tempFilePath,(err,result)=>{
-    console.log(result.url);
 
-  const claimemail = new Claimemail({
-    _id:new mongoose.sentAts.ObjectId(),
-
-
-    from:req.body.from,
-    mailgunResponseId:req.body.mailgunResponseId,
-    sentAt:req.body.sentAt,
-    subject:req.body.subject,
-    type:req.body.type,
-    to:req.body.to,
-    text:req.body.text,
-    
-
-    
-  })
-  claimemail.save()
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({
-        newClaimemail: result,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    })  
-  })
-});
 router.get("/:id", (req, res, next) => {
   console.log(req.params.id);
   Claimemail.findById(req.params.id).then(result=>{
